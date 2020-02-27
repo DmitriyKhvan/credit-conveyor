7172306811653bb00f029a6196db7e6af06ab5d3
Mashrabjon Akbarov <MAkbarov@nbu.local>
2020-02-27 20:14:55 +0500

    menu added

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,7 +1,16 @@
 <template>
-  <q-page-container>
-    <router-view />
-  </q-page-container>
+  <div>
+    <q-page-container>
+      <q-toolbar style="display: block;">
+        <q-breadcrumbs active-color="black" style="font-size: 16px">
+          <q-breadcrumbs-el label="Home" icon="home" />
+          <q-breadcrumbs-el label="Components" icon="widgets" />
+          <q-breadcrumbs-el label="Toolbar" />
+        </q-breadcrumbs>
+      </q-toolbar>
+      <router-view />
+    </q-page-container>
+  </div>
 </template>
 
 <script>
