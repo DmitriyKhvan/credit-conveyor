f37065be0a30ef573c43cd139ef530cd6b1033c8
freedom-twin <dimon.twin@gmail.com>
2020-03-14 16:07:30 +0500

    router fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -142,7 +142,7 @@ const routes = [{
                 path: "sub/profile",
                 name: "Profile",
                 component: CreditProfile,
-                beforeEnter: ifAuthenticated
+                //beforeEnter: ifAuthenticated
               }
             ]
           }
