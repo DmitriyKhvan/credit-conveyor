bb6d6a7861f167b3f9d1605cffc8cc3180ed6be3
Mashrabjon Akbarov <akbarov.dev@gmail.com>
2020-05-08 01:36:14 +0500

    active users monitoring and user addedit upgraded

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,5 +1,7 @@
 import store from '@/store/index';
 import NotifyService from './notify.service';
+import CommonUtils from '../shared/utils/CommonUtils';
+import { AuthService } from './auth.service';
 
 //let connection = store.getters["socket/usersCount"];
 //let socket = store.getters["socket/getSocket"];
@@ -19,12 +21,15 @@ const SocketService = {
       store.dispatch("socket/setUserCount", data);
     });
   },
-  runConnection(empId) {
+  runConnection(token) {
     let socket = store.getters["socket/getSocket"];
-    this.runNotifications(socket, empId);
+    this.runNotifications(socket);
     this.runChat(socket);
+    this.runOnline(socket, token);
+    this.runUserConnect(socket);
+    this.runUserDisconnect(socket);
+    this.runRemoveUser(socket);
 
-    socket.emit("online", empId);
     store.dispatch("socket/setOnline", true);
     console.log("user is online");
   },
@@ -43,7 +48,7 @@ const SocketService = {
       return true;
     }
   },
-  runNotifications(socket, empId) {
+  runNotifications(socket) {
     socket.on("notifications", data => {
       if (data) {
         data.forEach(msg => {
@@ -60,6 +65,42 @@ const SocketService = {
     socket.on("chat", data => {
       console.log(data);
     })
+  },
+  runOnline(socket, atoken) {
+    let uname = store.getters['auth/fullName'];
+    //let token = store.getters['auth/token'];
+    let empId = store.getters["auth/empId"]
+
+    let data = {
+      emp_id: empId,
+      emp_name: uname,
+      socket_id: socket.id,
+      token: atoken,
+      login_time: CommonUtils.formattedDate(new Date)
+    };
+
+    console.log({ online: data });
+    socket.emit("online", data);
+  },
+  runUserConnect(socket) {
+    socket.on("uconnect", usr => {
+      store.dispatch("auth/setActiveUsers", usr);
+    });
+  },
+  runUserDisconnect(socket) {
+    socket.on("udisconnect", socketId => {
+      store.dispatch("auth/disconnectActiveUser", socketId);
+    });
+  },
+  runRemoveUser(socket) {
+    socket.on("uremove", () => {
+      AuthService.logout();
+    });
+  },
+  killActiveUser(usr) {
+    let socket = store.getters["socket/getSocket"];
+    //console.log(usr);
+    socket.emit("uremove", usr);
   }
 };
 export default SocketService;
