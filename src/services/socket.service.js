45cc8187c8ae359ea5ec1c0777cb4962f12d4c48
Mashrabjon Akbarov <akbarov.dev@gmail.com>
2020-06-05 09:50:25 +0500

    active online

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -24,20 +24,24 @@ const SocketService = {
   runConnection() {
     let socket = store.getters["socket/getSocket"];
     let empId = store.getters["auth/empId"]
-    this.runNotifications(socket, empId);
+    //this.runNotifications(socket, empId);
     this.runChat(socket, empId)
     //this.runGroup(socket, empId)
     this.runChatList(socket, empId)
     this.runOnline(socket);
 
     socket.emit("chat/all", empId)
-    store.dispatch("socket/setOnline", true);
-    console.log("user is online");
+    //store.dispatch("socket/setOnline", true);
+    //console.log("user is online");
+
   },
   stopConnection() {
     let socket = store.getters["socket/getSocket"];
-    socket.emit("offline");
-    store.dispatch("socket/setOnline", false);
+    let empId = store.getters["auth/empId"];
+
+    socket.emit("offline", empId);
+
+    //store.dispatch("socket/setOnline", false);
     console.log("user is offline");
   },
   isOnline() {
@@ -77,18 +81,17 @@ const SocketService = {
     //   // logic...
     // });
   },
-  runUserConnect(socket) {
-    socket.on("uconnect", usr => {
+  runOnlineUsers(socket) {
+    socket.on("users/online", users => {
       let xlength = store.getters["auth/activeUsers"].length;
       console.length(xlength);
-
       usr.index = xlength + 1;
       store.dispatch("auth/setActiveUsers", usr);
     });
   },
-  runUserDisconnect(socket) {
-    socket.on("udisconnect", socketId => {
-      store.dispatch("auth/disconnectActiveUser", socketId);
+  runActiveUsers(socket) {
+    socket.on("users/active", users => {
+      store.dispatch("auth/disconnectActiveUser", users);
     });
   },
   runOnline(socket) {
