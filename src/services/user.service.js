1d0d599861ca64df246bfd9f116a1fa90c4419a7
Mashrabbek Akbarov <akbarov.dev@gmail.com>
2020-09-03 09:23:46 +0500

    img path change

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,15 @@
-import ApiService from './api.service'
-import TokenService from './storage.service'
+import ApiService from "./api.service";
+import TokenService from "./storage.service";
 import store from "@/store/index";
 
 const UserService = {
   getUserProfilePhotoUrl(emp_id) {
-    return `http://10.8.88.219/index.php?module=Tools&file=phones&prefix=profile&act=img&uid=${emp_id}`;
+    return `http://10.8.8.70:4000/emps/img/${emp_id}`;
   },
   isUserOnline(empId) {
     let userList = store.getters["auth/onlineUsers"];
     return userList.includes(empId) ? true : false;
   }
-}
+};
 
-export default UserService
+export default UserService;
