fa649cf9e80c57aa2eec388958a2933851c59289
mashrabjon <akbarov.dev@gmail.com>
2020-11-27 12:51:45 +0500

    extra-plugins

--- src/boot/extra-plugins.js
+++ src/boot/extra-plugins.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import IdleVue from 'idle-vue';
 import store from "@/store/index";
 
-const eventsHub = new Vue()
-const logoutTime = store.getters["auth/logoutTime"]
+// const eventsHub = new Vue()
+// const logoutTime = store.getters["auth/logoutTime"]
 
-const options = {
-  eventEmitter: eventsHub,
-  idleTime: logoutTime, // 10s
-  store
-};
+// const options = {
+//   eventEmitter: eventsHub,
+//   idleTime: logoutTime, // 10s
+//   store
+// };
 
-Vue.use(IdleVue, options);
+// Vue.use(IdleVue, options);
